a5c92d6bc24c694bdef2ec86d9c40240b279e1c4
Sehan Munir <sehanmunirgoldar@gmail.com>
2025-10-27 18:30:20 -0400

    Added Robot Vision
    Potentially robot vision, still requires testing and true usage of webcam.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMain.java
@@ -4,9 +4,25 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import java.util.Locale;
 
 @TeleOp(name = "RobotMain")
 public class RobotMain extends OpMode {
+
+    final boolean USING_WEBCAM = false;
+    final BuiltinCameraDirection INTERNAL_CAM_DIR = BuiltinCameraDirection.BACK;
+    final int RESOLUTION_WIDTH = 640;
+    final int RESOLUTION_HEIGHT = 480;
+
+    boolean lastX;
+    int frameCount;
+    long capReqTime;
+
+    VisionPortal portal;
+
     DcMotor motorTopLeft;
     DcMotor motorTopRight;
     DcMotor motorBottomLeft;
@@ -38,6 +54,53 @@ public class RobotMain extends OpMode {
         telemetry.addData("System Version", "1.0");
         telemetry.addData("Controller One", gamepad1.toString());
         telemetry.update();
+
+        VisionPortal portal;
+
+        if (USING_WEBCAM)
+        {
+            portal = new VisionPortal.Builder()
+                    .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                    .setCameraResolution(new Size(RESOLUTION_WIDTH, RESOLUTION_HEIGHT))
+                    .build();
+        }
+        else
+        {
+            portal = new VisionPortal.Builder()
+                    .setCamera(INTERNAL_CAM_DIR)
+                    .setCameraResolution(new Size(RESOLUTION_WIDTH, RESOLUTION_HEIGHT))
+                    .build();
+        }
+
+        while (!isStopRequested())
+        {
+            boolean x = gamepad1.x;
+
+            if (x && !lastX)
+            {
+                portal.saveNextFrameRaw(String.format(Locale.US, "CameraFrameCapture-%06d", frameCount++));
+                capReqTime = System.currentTimeMillis();
+            }
+
+            lastX = x;
+
+            telemetry.addLine("######## Camera Capture Utility ########");
+            telemetry.addLine(String.format(Locale.US, " > Resolution: %dx%d", RESOLUTION_WIDTH, RESOLUTION_HEIGHT));
+            telemetry.addLine(" > Press X (or Square) to capture a frame");
+            telemetry.addData(" > Camera Status", portal.getCameraState());
+
+            if (capReqTime != 0)
+            {
+                telemetry.addLine("\nCaptured Frame!");
+            }
+
+            if (capReqTime != 0 && System.currentTimeMillis() - capReqTime > 1000)
+            {
+                capReqTime = 0;
+            }
+
+            telemetry.update();
+        }
     }
 
     @Override
