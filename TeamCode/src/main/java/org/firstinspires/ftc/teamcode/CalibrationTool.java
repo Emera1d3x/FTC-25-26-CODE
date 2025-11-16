2724ce23c50bc6fad9b992fe83359448589f3e15
TinD <tindao2011@gmail.com>
2025-11-15 20:24:24 -0500

    Vision Tool to wrap up vision tasks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalibrationTool.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalibrationTool.java
@@ -5,4 +5,5 @@ public final class CalibrationTool {
     public static final double TICKS_PER_REV = 560.0;
     public static final int CAMERA_WIDTH = 640;
     public static final int CAMERA_HEIGHT = 480;
+    public static final boolean CAMERA_LIVE_VIEW = true;
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/VisionTool.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import static org.firstinspires.ftc.teamcode.CalibrationTool.*;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.opencv.core.*;
+import org.opencv.imgproc.*;
+
+import java.util.List;
+
+public class VisionTool {
+    public VisionTool(WebcamName webcam) {
+        ballProcessor = new InternalBallVisionProcessor();
+        tagProcessor = new AprilTagProcessor.Builder().build();
+        drawingProcessor = new InternalDrawingProcessor();
+        portal = new VisionPortal.Builder()
+                .setCamera(webcam)
+                .setCameraResolution(new android.util.Size(CAMERA_WIDTH, CAMERA_HEIGHT))
+                .addProcessors(ballProcessor, tagProcessor, drawingProcessor)
+                .enableLiveView(CAMERA_LIVE_VIEW)
+                .build();
+    }
+
+    public int getBallX() { return ballProcessor.getResult().getX(); }
+    public int getBallY() { return ballProcessor.getResult().getY(); }
+    public int getBallAngle() { return ballProcessor.getResult().getAngle(); }
+
+    public void addText(String label, String data) { drawingProcessor.addText(label, data); }
+    public void removeText(String label) { drawingProcessor.removeText(label); }
+
+    public List<AprilTagDetection> getTags() { return tagProcessor.getDetections(); }
+    public boolean isTagPresent(int id) {
+        for (AprilTagDetection detection : getTags()) {
+            if (detection.id == id)
+                return true;
+        }
+
+        return false;
+    }
+
+    public void shutdown() { portal.stopStreaming(); }
+
+    private final VisionPortal portal;
+    private final InternalBallVisionProcessor ballProcessor;
+    private final InternalDrawingProcessor drawingProcessor;
+    private final AprilTagProcessor tagProcessor;
+}
